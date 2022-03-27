c48a8cc64460b3b37294acaedb0d1a3f7e4065bc
happygoalvin <yo.alvin.96@gmail.com>
2022-03-27 19:58:53 +0800

    created SwordList class component and added navbar with logo using bootstrap

--- all-about-swords/src/App.js
+++ all-about-swords/src/App.js
@@ -1,12 +1,15 @@
-/* eslint-disable jsx-a11y/heading-has-content */
-import './App.css'
-import "bootstrap/dist/css/bootstrap.min.css"
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import SwordList from "./components/SwordList";
 
 function App() {
   return (
+    <React.Fragment>
     <div className="App">
-
-    </div>
+      <SwordList />
+      </div>
+    </React.Fragment>
   );
 }
 
---
+++ all-about-swords/src/components/SwordList.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default class swordList extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">
+      <img src="/images/sword-logo.svg" alt="" width="30" height="30" className="d-inline-block align-text-top" />
+      All-About-Swords
+    </a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+      </React.Fragment>
+    );
+  }
+}
