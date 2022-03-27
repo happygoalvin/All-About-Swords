ede3a78537a19a3b53130659b0ef7ee3f4b7f9f9
happygoalvin <yo.alvin.96@gmail.com>
2022-03-27 17:53:52 +0800

    installed and import bootstrap

--- all-about-swords/src/App.js
+++ all-about-swords/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/heading-has-content */
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
-    <React.Fragment>
-    
-    </React.Fragment>
+    <div className="App">
+
+    </div>
   );
 }
 
