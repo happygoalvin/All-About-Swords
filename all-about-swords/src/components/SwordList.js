749258ac65ffb0996f3ca2987f4ce7939038861e
happygoalvin <yo.alvin.96@gmail.com>
2022-03-30 00:29:53 +0800

    Successfully displayed data using cards in SwordList component

--- all-about-swords/src/components/SwordList.js
+++ all-about-swords/src/components/SwordList.js
@@ -1,42 +1,51 @@
 import React from "react";
 
 export default function swordList(props) {
-    console.log(props)
   return (
     <React.Fragment>
       <div className="container">
         <h1>Sword List</h1>
-        {props.data.map((r) => (
-          <React.Fragment key={r._id}>
-            <div className="col-sm-6">
-              <div className="row">
-                <div className="card" style={{width: "18rem"}}>
+        <div className="row">
+          {props.data.map((r) => (
+            <React.Fragment key={r._id}>
+              <div className="col">
+                <div className="card" style={{ width: "18rem" }}>
                   <img src={r.image_url} className="card-img-top" alt="..." />
                   <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </p>
+                    <h5 className="card-title">{r.name}</h5>
+                    <ul className="list-group list-group-flush">
+                      <li className="list-group-item">Origin: {r.origin}</li>
+                      <li className="list-group-item">
+                        Creation: {r.time_period_created}
+                      </li>
+                      <li className="list-group-item">
+                        Fighting styles: {r.fighting_style.join(", ")}
+                      </li>
+                    </ul>
+                    <p className="card-text">{r.description}</p>
+                    <ul className="list-group list-group-flush">
+                      <li className="list-group-item">
+                        Made of: {r.blade.metal}
+                      </li>
+                      <li className="list-group-item">
+                        Blade length: {r.blade.length}
+                        {r.blade.uom}
+                      </li>
+                    </ul>
                   </div>
-                  <ul class="list-group list-group-flush">
-                    <li class="list-group-item">An item</li>
-                    <li class="list-group-item">A second item</li>
-                    <li class="list-group-item">A third item</li>
-                  </ul>
-                  <div class="card-body">
-                    <a href="#" class="card-link">
+                  <div className="card-body">
+                    <a href="#" className="btn-sm btn-primary m-1">
                       Card link
                     </a>
-                    <a href="#" class="card-link">
+                    <a href="#" className="btn-sm btn-primary">
                       Another link
                     </a>
                   </div>
                 </div>
               </div>
-            </div>
-          </React.Fragment>
-        ))}
+            </React.Fragment>
+          ))}
+        </div>
       </div>
     </React.Fragment>
   );
